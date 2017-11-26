b10fb2d3690603b20ca97fccf3387c01bc3ca022
Joe Ibershoff <joe@ibershoff.com>
2017-11-26 16:30:14 -0500

    debugging

--- launch.sh
+++ launch.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-echo "PATH: $PATH"
-echo "meteor: "`which meteor`
-echo "npm: "`which npm`
-echo "ls: "`ls -la /app/.meteor/heroku_build/bin`
-
 # build client files
-meteor npm run meteor-client:bundle
+meteor-client bundle -s api
 
-# statically serve up all files from www; see server/doc-root.ts to see how index.html is handled as /
-cd api
-ln -s ../www public
-cd ..
 # mock the cordova.js file because the webapp doesn't need it, but still tries to load
 cat "// mock cordova.js file for webapp deployment" > ./www/cordova.js
 
+cd api
+
+# statically serve up all files from www; see server/doc-root.ts to see how index.html is handled as /
+ln -s ../www public
+
 # start the meteor server for API and (due to the above) static serving of the ionic client files
-meteor npm run api
+.meteor/heroku_build/bin/node .meteor/heroku_build/app/main.js
