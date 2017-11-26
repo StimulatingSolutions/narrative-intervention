f78e0c3e907ec4ce58c804287d8ddee29db1958c
Joe Ibershoff <joe@ibershoff.com>
2017-11-26 15:18:52 -0500

    try mv instead of ln

--- launch.sh
+++ launch.sh
@@ -5,11 +5,9 @@ set -e
 npm run meteor-client:bundle
 
 # statically serve up all files from www; see server/doc-root.ts to see how index.html is handled as /
-cd api
-ln -s ../www public
-cd ..
+mv www api/public
 # mock the cordova.js file because the webapp doesn't need it, but still tries to load it
-cat "// mock cordova.js file for webapp deployment" > ./www/cordova.js
+cat "// mock cordova.js file for webapp deployment" > ./api/public/cordova.js
 
 # start the meteor server for API and (due to the above) static serving of the ionic client files
 npm run api
