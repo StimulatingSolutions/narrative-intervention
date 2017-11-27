97e249d8fb1e39ae953faa087bd643651019f290
Joe Ibershoff <joe@ibershoff.com>
2017-11-26 23:36:22 -0500

    fixed embedded API URL

--- scripts/prod-unbundle.sh
+++ scripts/prod-unbundle.sh
@@ -3,9 +3,3 @@ set -e
 
 
 tar -xzvf client-bundle.tgz
-mv ./node_modules/meteor-client.prod.js ./node_modules/meteor-client.js
-
-
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-head -4 ./node_modules/meteor-client.js
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
