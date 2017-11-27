51105f4745cab1a75b1205494428f2ddd0eba355
Joe Ibershoff <joe@ibershoff.com>
2017-11-26 21:23:18 -0500

    debugging

---
+++ api/declaractions.d.ts
@@ -0,0 +1,1 @@
+../src/declaractions.d.ts
--- scripts/prod-launch.sh
+++ scripts/prod-launch.sh
@@ -2,13 +2,8 @@
 set -e
 
 
-echo "--------------------------------- app root"
-ls -lah .meteor/heroku_build/bin/node
-
-cd api
-
-echo "--------------------------------- inside api"
-ls -lah .meteor/heroku_build/bin/node
+echo "--------------------------------- compiled app"
+ls -lah .meteor/heroku_build/app/
 
 # start the meteor server for API and (due to the above) static serving of the ionic client files
 .meteor/heroku_build/bin/node .meteor/heroku_build/app/main.js
