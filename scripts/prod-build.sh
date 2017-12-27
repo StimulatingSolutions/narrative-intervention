c75b66ebf1d78723759ed695269bd9b1599059b0
Joe Ibershoff <joe@ibershoff.com>
2017-12-27 10:35:19 -0500

    cleaned APK

--- scripts/prod-build.sh
+++ scripts/prod-build.sh
@@ -8,12 +8,16 @@ then
   mv ./node_modules/meteor-client.js ./node_modules/meteor-client.dev.js
 fi
 
+rm -rf client-bundle.tgz
+
 npm run meteor-client:prod-bundle
 npm run ionic:build
 if [[ "$1" == "--build-apk" ]]
 then
   ionic cordova build android
   mv ./platforms/android/build/outputs/apk/android-debug.apk ./src/assets/narrative-intervention.apk
+  echo "removing unneeded files from APK"
+  zip ./src/assets/narrative-intervention.apk -q -d "assets/www/assets/*" "assets/www/build/*"
 fi
 
 tar -czvf client-bundle.tgz ./node_modules/meteor-client.js ./www ./src/pages/lesson-plans
