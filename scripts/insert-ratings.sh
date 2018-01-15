b2368fa6b0129e82bd407221649db11cacbe925b
Joe Ibershoff <joe@ibershoff.com>
2018-01-15 12:29:54 -0500

    insert ratings blocks

--- scripts/insert-ratings.sh
+++ scripts/insert-ratings.sh
@@ -7,4 +7,3 @@ LESSON_NUM=`printf "%02d\n" $1`
 
 mv ./src/pages/lesson-plans/lesson${LESSON_NUM}/lesson-${LESSON_NUM}.html $TMP_OUTPUT_DIR/Lesson-${LESSON_NUM}.tmp4.html
 cat $TMP_OUTPUT_DIR/Lesson-${LESSON_NUM}.tmp4.html | ts-node ./scripts/insert-ratings.ts ${LESSON_NUM} > ./src/pages/lesson-plans/lesson${LESSON_NUM}/lesson-${LESSON_NUM}.html
-cat ./src/pages/lesson-plans/lesson${LESSON_NUM}/lesson-${LESSON_NUM}.html | ts-node ./scripts/wrap-lesson-plan.ts ${LESSON_NUM} > ./src/pages/lesson-plans/standalone/lesson-${LESSON_NUM}.html
