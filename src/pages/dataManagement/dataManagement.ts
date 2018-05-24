b66065aa3b331e0e68234d46032a908b82e81e3f
Joe Ibershoff <joe@ibershoff.com>
2018-05-24 10:07:20 -0400

    clear confirmation and no-session rendering

--- src/pages/dataManagement/dataManagement.ts
+++ src/pages/dataManagement/dataManagement.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 //import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
-//import { AlertController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 import { MeteorObservable } from 'meteor-rxjs';
 
 import {ErrorAlert} from "../../services/errorAlert";
@@ -20,7 +20,7 @@ export class DataManagementPage extends DestructionAwareComponent implements OnI
 
   constructor(
     private errorAlert: ErrorAlert,
-    //private alertCtrl: AlertController,
+    private alertCtrl: AlertController,
     //private ref: ChangeDetectorRef,
   ) {
     super();
@@ -88,18 +88,46 @@ export class DataManagementPage extends DestructionAwareComponent implements OnI
 
   clear() {
     let ids: string[] = [];
+    let noDownloadCount = 0;
     let sessions: Session[] = Sessions.find({}, { sort: {creationTimestamp: 1, } }).fetch();
     for (let session of sessions) {
       if (this.checkboxes[session._id]) {
         ids.push(session._id);
+        if (!session.lastDownload) {
+          noDownloadCount++;
+        }
       }
     }
-    Meteor.call('clear', ids, (error, result) => {
-      if (error){
-        this.errorAlert.present(error, 32);
-        return;
+    let sessionAlert = this.alertCtrl.create();
+    sessionAlert.setCssClass('wide-input');
+    if (!ids.length) {
+      sessionAlert.setTitle('Oops!');
+      let subtitle: string = `No available sessions are selected; please select the sessions you want to delete.`;
+      sessionAlert.setSubTitle(subtitle);
+      sessionAlert.addButton('Ok');
+    } else {
+      sessionAlert.setTitle('Warning!');
+      let subtitle: string = `You are about to delete data for ${ids.length} sessions`;
+      if (noDownloadCount) {
+        subtitle += `, and ${noDownloadCount} of them ${noDownloadCount === 1 ? 'has' : 'have'} never been downloaded`;
       }
-    })
+      subtitle += '!';
+      sessionAlert.setSubTitle(subtitle);
+      sessionAlert.setMessage('Once you delete session data, it is gone for good, and cannot be recovered. Are you sure?');
+      sessionAlert.addButton('Cancel');
+      sessionAlert.addButton({
+        text: 'Ok',
+        handler: () => {
+          Meteor.call('clear', ids, (error, result) => {
+            if (error){
+              this.errorAlert.present(error, 32);
+              return;
+            }
+          })
+        }
+      });
+    }
+    sessionAlert.present();
   }
 
 }
