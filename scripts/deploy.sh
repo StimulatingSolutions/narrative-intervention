0a453919e3fc72a34dd147af856089f87d406438
Joe Ibershoff <joe@ibershoff.com>
2018-08-18 14:20:22 -0400

    deploying

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,12 @@ set -e
 
 MESSAGE=${1:-deploying}
 
+echo "###################################################### building for production"
 npm run prod:build
+echo "###################################################### committing files"
 git add .
 git commit -m "${MESSAGE}"
+echo "###################################################### pushing to github"
 git push origin master
+echo "###################################################### deploying to heroku"
 git push heroku master
--- scripts/prod-build.sh
+++ scripts/prod-build.sh
@@ -7,6 +7,7 @@ export IONIC_OPTIMIZE_JS=true
 
 if [ -e ./node_modules/meteor-client.js -a ! -e ./node_modules/meteor-client.dev.js ]
 then
+  echo "---------------------------- saving dev meteor-client bundle"
   rm -f ./node_modules/meteor-client.dev.js
   mv ./node_modules/meteor-client.js ./node_modules/meteor-client.dev.js
 fi
@@ -15,9 +16,11 @@ rm -rf client-bundle.tgz
 
 
 # monkey-patch ionic-app-scripts to work in prod mode with angular-meteor:
+echo "---------------------------- monkey-patching ionic-app-scripts"
 cp ./scripts/@ionic_app-scripts_dist_aot_utils.js ./node_modules/\@ionic/app-scripts/dist/aot/utils.js
 
 
+echo "---------------------------- hiding student-only code"
 cp ./src/app/select/app.component.teacher ./src/app/app.component.ts
 cp ./src/app/select/app.module.teacher ./src/app/app.module.ts
 mkdir -p srchide
@@ -28,6 +31,7 @@ then
 fi
 if [ ! -e ./src/pages/login ]
 then
+  echo "---------------------------- revealing teacher-only code"
   mv ./srchide/login ./src/pages
   mv ./srchide/landing ./src/pages
   mv ./srchide/dataManagement ./src/pages
@@ -41,23 +45,28 @@ then
 fi
 if [ ! -e ./node_modules/meteor-client.js ]
 then
+  echo "---------------------------- building prod meteor-client bundle"
   npm run meteor-client:prod-bundle
 fi
+echo "---------------------------- building prod teacher app"
 npm run ionic:build-prod
 rm -rf ./www/web
 mv ./www/build ./www/web
 cp ./src/tablet.html ./www/web/index.html
 
-mv ./srchide/studentSession ./src/pages
-mv ./srchide/studentLogin ./src/pages
-
 if [[ "$1" == "--build-apk" ]]
 then
+  echo "---------------------------- building native apk"
   ionic cordova build android
   mv ./platforms/android/build/outputs/apk/android-debug.apk ./src/assets/narrative-intervention.apk
   cp ./src/assets/narrative-intervention.apk ./www/assets/
 fi
 
+echo "---------------------------- revealing student-only code"
+mv ./srchide/studentSession ./src/pages
+mv ./srchide/studentLogin ./src/pages
+
+echo "---------------------------- hiding teacher-only code"
 cp ./src/app/select/app.component.student ./src/app/app.component.ts
 cp ./src/app/select/app.module.student ./src/app/app.module.ts
 mv ./src/pages/login ./srchide
@@ -70,11 +79,14 @@ mv ./src/pages/teacherSession ./srchide
 mv ./src/pages/usermanagement ./srchide
 mv ./src/pages/videos ./srchide
 mv ./src/services/email.ts ./srchide
+
+echo "---------------------------- building prod student app"
 npm run ionic:build-prod
 rm -rf ./www/student
 mv ./www/build ./www/student
 cp ./src/tablet.html ./www/student/index.html
 
+echo "---------------------------- revealing teacher-only code"
 mv ./srchide/login ./src/pages
 mv ./srchide/landing ./src/pages
 mv ./srchide/dataManagement ./src/pages
@@ -86,9 +98,10 @@ mv ./srchide/usermanagement ./src/pages
 mv ./srchide/videos ./src/pages
 mv ./srchide/email.ts ./src/services
 
+echo "---------------------------- installing app selector"
 cp ./src/redirect.html ./www/index.html
 cp ./src/redirect.html ./src/index.html
 
 
-echo "creating deployment tarball"
+echo "---------------------------- creating deployment tarball"
 tar -czvf client-bundle.tgz ./node_modules/meteor-client.js ./www  #./src/pages/lesson-plans
