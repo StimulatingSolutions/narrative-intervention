99bd4aa482d7d784157c6e85646c7ba13e69de0e
Joe Ibershoff <joe@ibershoff.com>
2018-11-13 14:30:25 -0500

    fixed password reset email

--- api/server/emailTemplates.ts
+++ api/server/emailTemplates.ts
@@ -1,29 +1,32 @@
+
+const SENDER = 'Narrative Structure <nsapp@stimulating-solutions.com>';
+
 export default function () {
   //SETUP ACCOUNTS EMAIL TEMPLATES
   Accounts.emailTemplates.siteName = 'Narrative Structure';
-  Accounts.emailTemplates.from = 'Narrative Structure <nsapp@stimulating-solutions.com>';
-  Accounts.emailTemplates.enrollAccount.subject = (user) => {
-    return `Welcome to the NS app, ${user.profile.name}`;
+  Accounts.emailTemplates.from = SENDER;
+  Accounts.emailTemplates.enrollAccount = {
+    subject(user) {
+      return `Welcome to the NS app, ${user.profile.name}`;
+    },
+    text(user, url) {
+      return 'You have been enrolled in the Narrative Structure app!'
+        + ' To activate your account, please click the link below:\n\n'
+        + url.replace('herokuapp', 'stimulating-solutions');
+    }
   };
-  Accounts.emailTemplates.enrollAccount.text = (user, url) => {
-    return 'You have been enrolled in the Narrative Structure app!'
-      + ' To activate your account, simply click the link below:\n\n'
-      + url.replace('herokuapp', 'stimulating-solutions');
-  };
-  Accounts.emailTemplates.resetPassword.subject = () => {
-    return `Your Narrative Structure password has been reset`;
-  };
-  Accounts.emailTemplates.resetPassword.from = () => {
-    // Overrides the value set in `Accounts.emailTemplates.from` when resetting
-    // passwords.
-    return 'NS Password Reset <nsapp@stimulating-solutions.com>';
-  };
-  Accounts.emailTemplates.verifyEmail = {
-     subject() {
-        return "Activate your account now!";
-     },
-     text(user, url) {
-        return `Hey ${user}! Verify your e-mail by following this link: ${url.replace('herokuapp', 'stimulating-solutions')}`;
-     }
+  Accounts.emailTemplates.resetPassword = {
+    subject(user) {
+      return `Your Narrative Structure password has been reset`;
+    },
+    text(user, url) {
+      return `Hello ${user.profile.name},\n` +
+        '\n' +
+        'To reset your password, please click the link below.\n' +
+        '\n' +
+        url.replace('herokuapp', 'stimulating-solutions')+'\n' +
+        '\n' +
+        'Thanks!\n';
+    }
   };
 }
--- src/util/backProtection.ts
+++ src/util/backProtection.ts
@@ -5,19 +5,21 @@ let backBusterCount: number = 1;
 function enableBackProtection(platform: Platform) {
   platform.registerBackButtonAction(() => {});
 
-  let currLoc: string = window.location.href.split('?')[0];
-  window.history.pushState(null, null, currLoc+`?backBuster${backBusterCount++}`);
-  window.history.pushState(null, null, currLoc+`?backBuster${backBusterCount++}`);
-  window.history.pushState(null, null, currLoc+`?backBuster${backBusterCount++}`);
-  window.history.pushState(null, null, currLoc+`?backBuster${backBusterCount++}`);
+  let parts: string[] = window.location.href.split('#');
+  let fragment: string = parts[1] ? '#'+parts[1] : '';
+  let currLoc = parts[0].split('?')[0];
+  window.history.pushState(null, null, currLoc+`?backBuster${backBusterCount++}${fragment}`);
+  window.history.pushState(null, null, currLoc+`?backBuster${backBusterCount++}${fragment}`);
+  window.history.pushState(null, null, currLoc+`?backBuster${backBusterCount++}${fragment}`);
+  window.history.pushState(null, null, currLoc+`?backBuster${backBusterCount++}${fragment}`);
   window.addEventListener('popstate', (event) => {
-    window.history.pushState(null, null, currLoc+`?backBuster${backBusterCount++}`);
-    window.history.pushState(null, null, currLoc+`?backBuster${backBusterCount++}`);
+    window.history.pushState(null, null, currLoc+`?backBuster${backBusterCount++}${fragment}`);
+    window.history.pushState(null, null, currLoc+`?backBuster${backBusterCount++}${fragment}`);
   });
   window.document.addEventListener("deviceready", () => {
     window.document.addEventListener("backbutton",() => {
-      window.history.pushState(null, null, currLoc+`?backBuster${backBusterCount++}`);
-      window.history.pushState(null, null, currLoc+`?backBuster${backBusterCount++}`);
+      window.history.pushState(null, null, currLoc+`?backBuster${backBusterCount++}${fragment}`);
+      window.history.pushState(null, null, currLoc+`?backBuster${backBusterCount++}${fragment}`);
       return false;
     }, false);
   }, false);
